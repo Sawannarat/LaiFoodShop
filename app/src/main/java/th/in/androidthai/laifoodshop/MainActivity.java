e107d26d78267df7c4fc94a3aba6ba916d6fc1ec
masterUNG <master@abc.com>
2019-05-06 21:49:45 -0700

    Initial commit

---
+++ app/src/main/java/th/in/androidthai/laifoodshop/MainActivity.java
@@ -0,0 +1,23 @@
+package th.in.androidthai.laifoodshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//add Fragment to Activity
+        if (savedInstanceState==null) {
+
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment,new AuthenFragment()).commit();
+
+        }
+
+
+    }//Main Method
+
+}//Main Class
