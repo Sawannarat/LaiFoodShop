0f54720136713a3a718df7496867b5f6de70557d
masterUNG <master@abc.com>
2019-05-08 00:18:59 -0700

    Register Sucess

---
+++ app/src/main/java/th/in/androidthai/laifoodshop/AddUserThread.java
@@ -0,0 +1,53 @@
+package th.in.androidthai.laifoodshop;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+public class AddUserThread extends AsyncTask<String, Void, String> {
+
+    private Context context;
+
+    public AddUserThread(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        try {
+            OkHttpClient okHttpClient = new OkHttpClient();
+
+            //Create Package
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("isAdd", "true")
+                    .add("Name", strings[0])
+                    .add("Gender", strings[1])
+                    .add("User", strings[2])
+                    .add("Password", strings[3])
+                    .build();
+          //  Create Request
+            Request .Builder builder = new Request.Builder();
+            Request request = builder.url(strings[4]) .post(requestBody) .build();
+
+           // Create Respone
+            Response response = okHttpClient .newCall(request).execute();
+            return response.body().string();
+
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+}
---
+++ app/src/main/java/th/in/androidthai/laifoodshop/MyConstant.java
@@ -0,0 +1,12 @@
+package th.in.androidthai.laifoodshop;
+
+public class MyConstant {
+
+
+    private String urlAddUser = "http://androidthai.in.th/ruts/addUserLai.php";
+    public String getUrlAddUser(){
+
+
+        return urlAddUser ;
+    }
+}
